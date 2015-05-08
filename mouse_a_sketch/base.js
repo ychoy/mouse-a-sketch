402bc528cb1f8b0359192e2d0a67150cca390afc
nathanallen <thenathanator@gmail.com>
2015-05-07 20:13:17 -0700

    simplification

--- mouse_a_sketch/base.js
+++ mouse_a_sketch/base.js
@@ -1,13 +1,9 @@
 $(document).ready(function(){
 
-  var $colorPicker = $("input"),
-      activeColor = $colorPicker.val();
-  
-  $colorPicker.on("change", function(){
-    activeColor = $colorPicker.val();
-  })
+  var $colorPicker = $("input");
 
   $("td").on("mouseenter", function(){
-    $(this).css("background-color", activeColor);
+    $(this).css("background-color", $colorPicker.val());
   })
+  
 })
