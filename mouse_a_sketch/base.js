5e055014cfd206c0d8ec44790d8dd4af27c59d66
nathanallen <thenathanator@gmail.com>
2015-05-18 21:45:37 -0700

    generate grid based on window size

--- mouse_a_sketch/base.js
+++ mouse_a_sketch/base.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
 
+  var w = $(window).width();
+  var h = $(window).height();
+
+  buildGrid(w,h);
+
   var $colorPicker = $("input"),
       activeColor = $colorPicker.val();
   
@@ -16,3 +21,8 @@ $(document).ready(function(){
 
 });
 
+function buildGrid(w,h){
+  var row = "<tr>" + Array(Math.floor(w/10)).join("<td></td>") + "</tr>";
+  var rows = Array(Math.floor(h/10)).join(row);
+  $("table").html(rows)
+}
