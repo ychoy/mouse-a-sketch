40f400632fbba5ab212a682b213c2aa3cecfc5c5
ychoy <yanyinchoy@gmail.com>
2017-02-03 12:03:38 -0800

    edited jquery to select color for drawing

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,10 @@
 console.log("this is a sanity check");
 
 $(document).ready(function(){
-  console.log("Let's get coding!")
- 	$('td').hover(function(){
-		$(this).css("background", "#380606");
+ 	$('td').mouseenter(function(){
+		$(this).css("background", ($("input").val()));
 	});
 
+	// bonus - spacebar to turn off and on drawing
+	// second bonus - fix space bar/ color picker problem (hint in rot-13: sbphf / oyhe)
 })
