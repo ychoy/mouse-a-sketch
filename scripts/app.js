715b3e9e96758b9b10cfb260fb8ab4a903b4865c
ychoy <yanyinchoy@gmail.com>
2017-02-03 11:35:56 -0800

    jquery to draw when mouse hovers

--- scripts/app.js
+++ scripts/app.js
@@ -2,4 +2,8 @@ console.log("this is a sanity check");
 
 $(document).ready(function(){
   console.log("Let's get coding!")
+ 	$('td').hover(function(){
+		$(this).css("background", "#380606");
+	});
+
 })
